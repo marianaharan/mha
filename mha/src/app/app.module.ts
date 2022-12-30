2dc9624ae1c5959575a44518b16cbbbec5880290
marianaharan <121615793+marianaharan@users.noreply.github.com>
2022-12-30 11:01:31 -0300

    Update de front end

--- mha/src/app/app.module.ts
+++ mha/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -22,7 +24,9 @@ import { HysComponent } from './components/hys/hys.component';
     AcercadeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
